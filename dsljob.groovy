16b3a25ffa917386eb36933933fe83a5699be36e
Jose Antonio Cervantes <85256588+jacfpentester@users.noreply.github.com>
2023-05-09 00:43:00 +0200

    Update dsljob.groovy

--- dsljob.groovy
+++ dsljob.groovy
@@ -1,1 +1,22 @@
-
+job('EjecucionRemotaScript1'){
+  description('Job DSL que ejecuta el script prueba.sh de github')
+  scm{
+    git('https://github.com/jacfpentester/jenkinsscript.git', 'master'){
+      node ->
+        node / gitConfigName('jacfpentester')
+        node / gitConfigEmail('jacfpentester@gmail.com')
+    }
+  }
+  parameters{
+    choiceParam('opciones',['Opcion 1', 'Opcion2', 'Opcion3 (default)'] )
+  }
+  triggers {
+    cron('H/1 * * * *')
+  }
+  steps {
+    shell("cat hola.txt")
+  }
+  publishers {
+    mailer('jacfpentester@gmail.com',true,true)
+  }
+}
